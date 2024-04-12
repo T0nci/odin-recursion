4d36484044720190c454844a5f1685956d631f27
T0nci <138928016+T0nci@users.noreply.github.com>
2024-04-12 23:58:02 +0200

    Complete recursive merge sort exercise

---
+++ merge-sort.js
@@ -0,0 +1,28 @@
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const left = mergeSort(arr.slice(0, Math.ceil(arr.length / 2)));
+  const right = mergeSort(arr.slice(Math.ceil(arr.length / 2), ));
+
+  const mergedSortedArray = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      mergedSortedArray.push(left.shift());
+    } else {
+      mergedSortedArray.push(right.shift());
+    }
+  }
+
+  if (!left.length) {
+    return mergedSortedArray.concat(right);
+  } else if (!right.length) {
+    return mergedSortedArray.concat(left);
+  }
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
+console.log(mergeSort([3, 2, 1]));
